2ab8134c06ac5303c7a44100e1959718511f0a39
ishu-ig <ishaanguptacse@gmail.com>
2025-06-27 21:48:12 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -33,8 +33,6 @@ app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
-const path = require('path');
-
 // Serve admin panel
 app.use('/admin', express.static(path.join(__dirname, 'admin/build')));
 
